32075309fa51fb076868954f48dd6ef70776de3a
RobinSrk <robinsrk3@gmail.com>
2024-12-09 01:52:33 +0600

    new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.robin.flip_2_dnd"
         minSdk = 23
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
         vectorDrawables {
             useSupportLibrary = true
         }

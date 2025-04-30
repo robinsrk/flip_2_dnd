ce52799b2d3c5e5661872bb2d96adb87fd515bf2
Robin <robinsrk3@gmail.com>
2025-05-01 02:57:37 +0600

    v4.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
 		applicationId = "dev.robin.flip_2_dnd"
 		minSdk = 23
 		targetSdk = 34
-		versionCode = 311
-		versionName = "3.1.1"
+		versionCode = 400
+		versionName = "4.0.0"
 		vectorDrawables {
 			useSupportLibrary = true
 		}

df7e38aa36df65a2bd7b054bf2dc4cb6c75204af
Robin <robinsrk3@gmail.com>
2026-01-18 15:17:05 +0600

    chore: Release 9.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "dev.robin.flip_2_dnd"
 		minSdk = 23
 		targetSdk = 35
-		versionCode = 901
-		versionName = "9.0.1"
+		versionCode = 902
+		versionName = "9.0.2"
 		vectorDrawables {
 			useSupportLibrary = true
 		}

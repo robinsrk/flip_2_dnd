fef16fdd06a16eb8a3fcf7fdd71945686549c4cb
Robin <robinsrk3@gmail.com>
2026-02-27 14:09:07 +0600

    chore: bump version to 11.1.2 (1112) and update changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
 		applicationId = "dev.robin.flip_2_dnd"
 		minSdk = 23
 		targetSdk = 35
-		versionCode = 1111
-		versionName = "11.1.1"
+		versionCode = 1112
+		versionName = "11.1.2"
 		vectorDrawables {
 			useSupportLibrary = true
 		}

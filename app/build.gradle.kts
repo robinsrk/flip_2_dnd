45b8b8aa9d7f0953f46080769d80139655926eb4
Robin <robinsrk3@gmail.com>
2026-03-02 04:28:57 +0600

    chore: bump version to 12.0.1 (1201) and update changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "dev.robin.flip_2_dnd"
 		minSdk = 23
 		targetSdk = 35
-		versionCode = 1200
-		versionName = "12.0.0"
+		versionCode = 1201
+		versionName = "12.0.1"
 		vectorDrawables {
 			useSupportLibrary = true
 		}

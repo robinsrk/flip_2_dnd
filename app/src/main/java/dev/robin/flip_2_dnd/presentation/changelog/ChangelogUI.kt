72947f3521d4e4d0b0e1c37ae28f99f0b3092937
Robin <robinsrk3@gmail.com>
2026-03-14 00:00:03 +0600

    chore: bump version to 12.0.5 (1205) and update changelogs

--- app/src/main/java/dev/robin/flip_2_dnd/presentation/changelog/ChangelogUI.kt
+++ app/src/main/java/dev/robin/flip_2_dnd/presentation/changelog/ChangelogUI.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-// Removed heavy icon dependencies; using simple unicode chevrons instead
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme

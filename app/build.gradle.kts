a8366861712189a65f80ced7637d8e2806632660
Robin <robinsrk3@gmail.com>
2026-03-14 00:37:19 +0600

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "dev.robin.flip_2_dnd"
 		minSdk = 23
 		targetSdk = 35
-		versionCode = 1205
-		versionName = "12.0.5"
+		versionCode = 1206
+		versionName = "12.0.6"
 		vectorDrawables {
 			useSupportLibrary = true
 		}
